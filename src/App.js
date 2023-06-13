981a9334096180c1043d83f07674185300c22a23
MUDIT ARORA <muditarora26@gmail.com>
2023-06-13 16:23:00 +0530

    UseState | Custom events | Custom Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import AnimalShow from './AnimalShow';
 import 'bulma/css/bulma.css';
+import { useState } from 'react';
 
 function App() {
+    
+    const [count, setCount] = useState(0);
     const handleClick = () => {
         alert("This is a animal");
     }
+    const handleClickCount = () => {
+        // set the count 
+        setCount(count + 1); 
+    }
+    
+    //custom function for alert button
+    function AlertButton({message, children}) {
+        return (
+            <button onClick={() => alert(message)}>{children}</button>
+        );
+    }
+    
     return (
         
         <div>
@@ -12,8 +27,16 @@ function App() {
             <br></br>
             <br></br>
             <button className='button is-grey ml-6' onClick={ () => alert("Button is Clicked by Lion")}>Add Animal - Arrow Function</button>
+            <br></br>
+            <br></br>
+            <button className='button is-red ml-6 buttons' onClick={handleClickCount}>Add Animal Count</button>
+            <div>No of animals is: {count}</div>
 
-            Animals List is here 
+            <div>********************************************* Buttons using custom default AlertButton*******************************************************</div>
+            <AlertButton message="Hey Mudit! Playing">Play Movie</AlertButton>
+            <AlertButton message="Uploading the Files! Please wait for a while">Upload Files</AlertButton>
+            
+    
         </div>
     )
 }
