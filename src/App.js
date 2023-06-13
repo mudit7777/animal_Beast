8fc3961fa6cb5197fe48d5df94ec88dd53268b6b
MUDIT ARORA <muditarora26@gmail.com>
2023-06-13 13:51:02 +0530

    EventHandler
    Event call various methods

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import 'bulma/css/bulma.css';
 
 function App() {
     const handleClick = () => {
-        console.log("This is a animal");
+        alert("This is a animal");
     }
     return (
+        
         <div>
-            <button className='button is-black' onClick={handleClick}>Add Animal</button>
+            <button className='button is-black ml-6' onClick={function() {alert("Button is Clicked by Mudit")}}>Add Animal</button>
+            <br></br>
+            <br></br>
+            <button className='button is-grey ml-6' onClick={ () => alert("Button is Clicked by Lion")}>Add Animal - Arrow Function</button>
+
             Animals List is here 
         </div>
     )
