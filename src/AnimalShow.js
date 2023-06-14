e15e1855973af98da28cbb162114e49ef92e4f32
MUDIT ARORA <muditarora26@gmail.com>
2023-06-14 15:46:14 +0530

    AnimalBeast App Done
    Done with more props n styling

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,7 +1,38 @@
-function AnimalShow() {
-    <div>
-        Lion!
+import './AnimalShow.css'
+import { useState } from 'react'
+import bird from './svg/bird.svg'
+import cat from './svg/cat.svg'
+import cow from './svg/cow.svg'
+import dog from './svg/dog.svg'
+import gator from './svg/gator.svg'
+import heart from './svg/heart.svg'
+import horse from './svg/horse.svg'
+
+// do the map of the SVG'S 
+
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    horse
+}
+
+
+
+function AnimalShow({ type }) {
+    const [clicks, setClicks] = useState(0);
+    // handle the no of clicks
+    const handleClicks = () => {
+        return setClicks(clicks + 1);
+    }
+   return (
+    <div className="animal-show">
+        <img alt="animal" className='animal' src={svgMap[type]} /> 
+        <img alt="heart" className='heart' onClick={handleClicks} src={heart} style={{width:10 + 10*clicks + "px"}}/>         
     </div>
+   );
 }
 
 export default AnimalShow;
--- src/App.js
+++ src/App.js
@@ -1,43 +1,31 @@
+import './App.css';
+import { render } from '@testing-library/react';
 import AnimalShow from './AnimalShow';
 import 'bulma/css/bulma.css';
 import { useState } from 'react';
+function getRandomAnimal() {
+    const animals = ['bird','cat','cow', 'dog', 'gator', 'horse'];
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
+//console.log(getRandomAnimal());
 
 function App() {
-    
-    const [count, setCount] = useState(0);
+    const [animals, setAnimals] = useState([]);
+  
     const handleClick = () => {
-        alert("This is a animal");
-    }
-    const handleClickCount = () => {
-        // set the count 
-        setCount(count + 1); 
-    }
-    
-    //custom function for alert button
-    function AlertButton({message, children}) {
-        return (
-            <button onClick={() => alert(message)}>{children}</button>
-        );
-    }
-    
-    return (
-        
-        <div>
-            <button className='button is-black ml-6' onClick={function() {alert("Button is Clicked by Mudit")}}>Add Animal</button>
-            <br></br>
-            <br></br>
-            <button className='button is-grey ml-6' onClick={ () => alert("Button is Clicked by Lion")}>Add Animal - Arrow Function</button>
-            <br></br>
-            <br></br>
-            <button className='button is-red ml-6 buttons' onClick={handleClickCount}>Add Animal Count</button>
-            <div>No of animals is: {count}</div>
+    //animals.push(getRandomAnimal()); // this code modifies a piece of state and we never want to modify the state.
+    setAnimals([...animals, getRandomAnimal()])
+    };
 
-            <div>********************************************* Buttons using custom default AlertButton*******************************************************</div>
-            <AlertButton message="Hey Mudit! Playing">Play Movie</AlertButton>
-            <AlertButton message="Uploading the Files! Please wait for a while">Upload Files</AlertButton>
-            
-    
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index}/>   })
+
+    return (
+        <div className="app">
+            <button onClick={handleClick}>Add Animal</button>
+            <div className="animal-list">{renderedAnimals}</div>
         </div>
-    )
+    );
 }
 export default App;
